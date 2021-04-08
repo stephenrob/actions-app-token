f994b5d677f8170c18cfa7a0585835615c05fc0e
Stephen Robinson <stephenrob@users.noreply.github.com>
2021-04-08 17:13:38 +0000

    Add logging

--- index.js
+++ index.js
@@ -21,16 +21,18 @@ async function run() {
     let installationTokens = []
 
     for await (const { octokit, installation } of app.eachInstallation.iterator()) {
+      core.info('retrieving token for installation: ' + installation.id)
       const resp = await octokit.auth({
         type: 'installation',
         installationId: installation.id
       })
       installationTokens.push(resp.token)
+      core.info('retrieved token for installation: ' + installation.id)
     }
 
     const token = installationTokens[0];
 
-    core.debug('token: ' + token)
+    core.info('token: ' + token)
 
     core.setOutput('token', token);
   } catch (error) {
